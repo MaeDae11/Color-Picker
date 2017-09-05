ea306c7cbcb6ab4bfb999d6d2671c78e1e2efc33
Katie <katherinelane11@gmail.com>
2017-09-05 18:30:29 -0400

    Adds comments to random.js

--- src/components/Random.js
+++ src/components/Random.js
@@ -8,28 +8,29 @@ class Random extends Component {
         this.state = { color: [0, 25, 237]}
         this.handleClick = this.handleClick.bind(this);
     }
+    // componentDidMount() is invoked immediately after a component is mounted. Initialization that requires DOM nodes should go here. If you need to load data from a remote endpoint, this is a good place to instantiate the network request. Setting state in this method will trigger a re-rendering. 
     componentDidMount() {
         this.applyColor();
     }
-    
+    // componentWillUpdate() is invoked immediately before rendering when new props or state are being received. Use this as an opportunity to perform preparation before an update occurs. This method is not called for the initial render.
     componentDidUpdate(prevProps, prevState) {
         this.applyColor();
     }
-
+    //formats 'rgb' for DOM
     formatColor(ary) {
         return 'rgb(' + ary.join(', ') + ')';
     }
-
+    //adjusts button color based on this.state.color for background
     isLight() {
         const rgb = this.state.color;
         return rgb.reduce((a,b) => a+b) < 127 * 3;
     }
-
+    // applies background color to body of document
     applyColor() {
         const color = this.formatColor(this.state.color);
         document.body.style.background = color;
     }
-
+    //chooses random numbers for rgb rendering
     chooseColor() {
         const random = [];
         for (let i = 0; i < 3; i++) {
@@ -37,6 +38,7 @@ class Random extends Component {
         }
         return random;
     }
+    // click handler event to choose color when button is clicked
     handleClick(){
         this.setState({ 
           color: this.chooseColor()
