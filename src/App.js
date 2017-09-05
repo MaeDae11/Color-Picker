ba2b7f85830b73f732737193ebe595a214178c0e
Katie <katherinelane11@gmail.com>
2017-09-05 17:58:01 -0400

    shell of button and random js / imports js files to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './components/Button.js';
+import Random from './components/Random.js';
+
+
 class App extends Component {
   render() {
     return (
